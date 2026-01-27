bbd72221cbc0d1e37ad7cd6b28a572b8ad5ec7d1
Rajdeep <rajdeep.hascode@gmail.com>
2026-01-27 23:21:25 +0530

    Added dotenv for client in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,13 +13,13 @@ export default defineConfig({
       // 1. Matches any request starting with "/api"
       '/api': {
         // 2. Forwards it to the Backend Server
-        target: import.meta.env.VITE_API_URL || 'https://chatflow-67xw.onrender.com', 
+        target: process.env.VITE_API_URL || 'https://chatflow-67xw.onrender.com', 
         changeOrigin: true,
         secure: false,
       },
       // 2. Proxy Socket.io (CRITICAL STEP)
       '/socket.io': {
-        target: import.meta.env.VITE_API_URL || 'https://chatflow-67xw.onrender.com',
+        target: process.env.VITE_API_URL || 'https://chatflow-67xw.onrender.com',
         ws: true, // Enables WebSocket proxying
         changeOrigin: true,
         secure: false,
